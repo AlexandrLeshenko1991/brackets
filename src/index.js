832a7f9af1c269213f3a774c8933275a9819d2f3
Alex Leshenko <alex.l@sca.auction>
2020-03-09 11:35:25 +0300

    feat: task Brackets complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let patrn = bracketsConfig.map(i=>i.join(''))
+    let result = str;
+
+    let nArr = [true]
+
+    while (nArr.includes(true) && result.length){
+        for (let i = 0; i < patrn.length; i++ ){
+            result = result.replace(patrn[i],'')
+        }
+        nArr = patrn.map(p=>{
+            if (result.indexOf(p) < 0) {
+                return false
+            }else {
+                return true
+            }
+        })
+    }
+
+    return !result
+
 }
